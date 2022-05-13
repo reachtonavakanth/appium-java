0385427c0b05f257d642fa02507326ed36e08a59
reachtonavakanth <mailtonavakanth@gmail.com>
2022-05-13 23:40:32 +0530

    added reading from json

---
+++ src/test/java/com/qa/tests/ReadData.java
@@ -0,0 +1,67 @@
+package com.qa.tests;
+
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.io.File;
+
+public class ReadData {
+    public static String path;
+
+    @Test
+    public void readJsonFileData() throws FileNotFoundException {
+        path = System.getProperty("user.dir") + File.separator + "src" +
+                File.separator + "test" + File.separator + "resources" + File.separator + "testdata" +
+                File.separator + "Sample.json";
+
+        System.out.println(path);
+        readfromFile();
+
+    }
+
+    private static void readfromFile() throws FileNotFoundException {
+        //FileReader file = new FileReader(path);
+
+        InputStream inputstream = new FileInputStream(path);
+
+
+        JSONTokener tokener = new JSONTokener(inputstream);
+        JSONObject jObj = new JSONObject(tokener);
+        System.out.println(jObj.getString("key").toString());
+
+        System.out.println(jObj.getJSONObject("TC_01"));
+
+        System.out.println(jObj.getJSONObject("TC_01").getString("username"));
+        System.out.println(jObj.getJSONObject("TC_01").getString("password"));
+        System.out.println(jObj.getJSONObject("TC_02").getString("username"));
+        System.out.println(jObj.getJSONObject("TC_02").getString("password"));
+        System.out.println(jObj.getJSONObject("TC_03").getString("username"));
+        System.out.println(jObj.getJSONObject("TC_03").getString("password"));
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(0).getString("firstName"));
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(0).getString("lastName"));
+
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(1).getString("firstName"));
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(1).getString("lastName"));
+
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(2).getString("firstName"));
+        System.out.println(jObj.getJSONArray("employees").getJSONObject(2).getString("lastName"));
+
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(0).getString("firstName"));
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(0).getString("lastName"));
+
+
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(1).getString("firstName"));
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(1).getString("lastName"));
+
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(2).getString("firstName"));
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(2).getString("lastName"));
+
+        System.out.println(jObj.getJSONArray("manager").getJSONObject(2));
+        System.out.println(jObj.getJSONArray("manager"));
+
+
+    }
+}
